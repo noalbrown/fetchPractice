60764f2b9b9e10098f2dcf9b7caf4e935e28d85c
Noal Brown <noalbrown1@gmail.com>
2020-11-09 15:50:18 -0600

    changed to arrow functions

--- app.js
+++ app.js
@@ -4,24 +4,18 @@ document.getElementById('button3').addEventListener('click', getExternal);
 
 function getText() {
   fetch('test.txt')
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (data) {
+    .then(res => res.text())
+    .then(data => {
       console.log(data)
       document.getElementById('output').innerHTML = data;
     })
-    .catch(function (err) {
-      console.log(err)
-    })
+    .catch(err => console.log(err))
 }
 
 function getJson() {
   fetch('posts.json')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then((res) => res.json())
+    .then(data => {
       console.log(data)
       let output = '';
       data.forEach(function (post) {
@@ -29,25 +23,17 @@ function getJson() {
       })
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err)
-    })
+    .catch(err => console.log(err))
 }
 
 function getExternal() {
   fetch('https://api.github.com/users')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data)
       let output = '';
-      data.forEach(function (user) {
-        output += `<li>${user.login}</li>`
-      })
+      data.forEach(user => output += `<li>${user.login}</li>`)
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err)
-    })
+    .catch(err => console.log(err))
 }
